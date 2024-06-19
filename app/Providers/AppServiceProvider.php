b32f968dccd9e514b7c20ca33d4a67fce33a754d
jison-jpeg <2101101090@student.buksu.edu.ph>
2024-06-19 19:39:43 +0800

    [FE] - Added Table for User Management

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::component('components.header', 'header');
+        Blade::component('components.sidebar', 'sidebar');
+        Blade::component('components.footer', 'footer');
+        Blade::component('components.search-bar', 'search-bar');
+        Blade::component('components.notifications', 'notifications');
+        Blade::component('components.messages', 'messages');
+        Blade::component('components.profile', 'profile');
+        // Table
+        Blade::component('components.tables.user', 'tables-user');
     }
 }
