165e72228c41b23a6c9b573cf197e38fcdb52331
jison-jpeg <2101101090@student.buksu.edu.ph>
2024-06-19 20:48:53 +0800

    [FE] Added Edit and Delete Modal

--- resources/views/components/modals/user.blade.php
+++ resources/views/components/modals/user.blade.php
@@ -19,8 +19,8 @@
                     </div>
                     <div class="col-md-4">
                         <label for="middle_name" class="form-label">Middle Name</label>
-                        <input type="text" class="form-control" name="middle_name" placeholder="Enter your middle name"
-                            required>
+                        <input type="text" class="form-control" name="middle_name"
+                            placeholder="Enter your middle name" required>
                         <div class="invalid-feedback">
                             Please enter your middle name.
                         </div>
@@ -35,7 +35,8 @@
                     </div>
                     <div class="col-md-12">
                         <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control" name="email" placeholder="Enter your email" required>
+                        <input type="email" class="form-control" name="email" placeholder="Enter your email"
+                            required>
                         <div class="invalid-feedback">
                             Please enter your email.
                         </div>
@@ -51,5 +52,75 @@
 </div>
 
 {{-- Edit Modal --}}
+<div class="modal fade" id="editModal" tabindex="-1">
+    <div class="modal-dialog modal-xl modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Edit User</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form class="row g-3 needs-validation" method="POST" novalidate>
+                    @csrf
+                    @method('PATCH')
+                    <div class="col-md-4">
+                        <label for="first_name" class="form-label">First Name</label>
+                        <input type="text" class="form-control" name="first_name" placeholder="Enter your first name"
+                            required>
+                        <div class="invalid-feedback">
+                            Please enter your first name.
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <label for="middle_name" class="form-label">Middle Name</label>
+                        <input type="text" class="form-control" name="middle_name"
+                            placeholder="Enter your middle name" required>
+                        <div class="invalid-feedback">
+                            Please enter your middle name.
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <label for="last_name" class="form-label">Last Name</label>
+                        <input type="text" class="form-control" name="last_name" placeholder="Enter your last name"
+                            required>
+                        <div class="invalid-feedback">
+                            Please enter your last name.
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" name="email" placeholder="Enter your email"
+                            required>
+                        <div class="invalid-feedback">
+                            Please enter your email.
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="reset" class="btn btn-secondary">Reset</button>
+                        <button type="submit" class="btn btn-primary">Confirm</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 {{-- Delete Modal --}}
+<div class="modal fade" id="deleteModal" tabindex="-1">
+    <div class="modal-dialog modal-xl modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Delete User</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p class="text-center">Are you sure you want to delete this user?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-danger">Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
