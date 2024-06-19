307a4d5d736c39585508c9b74ed5357e2856dc86
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-19 21:16:26 +0800

    feat: purified oil model, controller, migration

---
+++ app/Http/Controllers/PurifiedOilController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PurifiedOil;
+use Illuminate\Http\Request;
+
+class PurifiedOilController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PurifiedOil $purifiedOil)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PurifiedOil $purifiedOil)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PurifiedOil $purifiedOil)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PurifiedOil $purifiedOil)
+    {
+        //
+    }
+}
