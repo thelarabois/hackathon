21af0a4ff3320d2141be788eacf90ea04f989bbc
Kirk John L. Sieras <bebekirk123@gmail.com>
2024-06-19 17:56:31 +0800

    add some specific folders in controller

---
+++ app/Http/Controllers/Super_admin/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function add(Request $request)
+    {
+
+    }
+
+    public function edit(string $id)
+    {
+
+    }
+
+    public function delete(string $id)
+    {
+
+    }
+}
